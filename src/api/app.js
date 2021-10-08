20d173bbff93ead489156e57decc57637a852567
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-08 16:47:29 -0300

    add endpoint get /employees

--- src/api/app.js
+++ src/api/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const routes = require('../routes');
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/employees', routes.EmployeeRouter);
 
 module.exports = app;
